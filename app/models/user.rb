605e97d43337876455ed3455a8d1d072c32b5a0f
Marcos Flach <marcosoftn1@gmail.com>
2024-02-28 16:34:29 -0300

    Add cars and orders models

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,10 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # we code dependent detroy because when someone delete her or his profile
+  # the cars that belongs to the profile go out with them.
+  has_many :cars, dependent: :destroy
+
+  has_many :orders
 end
