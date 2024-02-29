553fa9a12327d86ed7254045a9fd48d4a8095829
Felipe Sayeg <felipesayeg2018@gmail.com>
2024-02-29 14:08:22 -0300

    show done

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,14 +1,14 @@
 class CarsController < ApplicationController
 
 
-  skip_before_action :authenticate_user!, only: :index
+  skip_before_action :authenticate_user!, only: [:index, :show]
 
   def index
     @cars = Car.all
   end
 
   def show
-    @car = Car.find(params(:id))
+    @car = Car.find(params[:id])
   end
 
   def new
