2b061cce890679d5323f11a3aeabdc3b35399e00
Felipe Sayeg <felipesayeg2018@gmail.com>
2024-02-29 07:54:06 -0300

    longing

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,11 @@
 class CarsController < ApplicationController
+  skip_before_action :authenticate_user!, only: :index
+def index
+@cars = Car.all
+end
+
+def show
+@car = Car.find(params(:id))
+end
+
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,19 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+Car.create!(
+  year: 2020,
+  brand: "Toyota",
+  description: "Carro sedan confortável",
+  price: 25000.0,
+  # user_id: 1,
+  created_at: Time.now,
+  updated_at: Time.now
+)
+
+Car.create!(
+  year: 2018,
+  brand: "Honda",
+  description: "Carro SUV espaçoso",
+  price: 30000.0,
+  # user_id: 1,
+  created_at: Time.now,
+  updated_at: Time.now
+)
